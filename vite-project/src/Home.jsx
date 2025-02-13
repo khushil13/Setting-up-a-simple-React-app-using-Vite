f453e883523929d35b3713d8204ce7194736966f
TIGER KILLER <choprakhushil13@gmail.com>
2025-02-13 09:58:39 +0530

    updated

---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            
+            <h1>Welcome to your new React app!</h1>
+            <p>Name:    Your Name</p>
+            <p>Email:   your email</p>
+            <p>Contact: your phone number</p>
+        </div>
+    );
+};
+export default Home;
